0a683d0a08f9c5829c3def421539838c67435d21
Jean McLeod <abricsltd@gmail.com>
2022-01-09 21:18:17 +0000

    draftfinal

--- script.js
+++ script.js
@@ -10,10 +10,10 @@
 
 var generateBtn = document.querySelector("#generate");
 
-const numbers=[0,1,2,3,4,5,6,7,8,9]
-const uppercase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const lowercase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const symbol=["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ];
+let numbers= "0123456789";
+let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase="abcdefghijklmnopqrstuvwxyz";
+let symbol="!#$%&()*+,-./:;<=>?@[]^_`{|}"
 let pwGen = "";
 
 
@@ -91,7 +91,7 @@ return pwGen;
 
 //placing the closing bracket here brought the passwordText.Value back to bright blue
 }
-// passwordText.value = password;
+
 }
 // Add event listener to generate button.
 generateBtn.addEventListener("click", generatePassword)
